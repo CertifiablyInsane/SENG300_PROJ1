5e73aacaf48cd519e21bffab5ef75b35e522a50d
jw1804 <124788631+jw1804@users.noreply.github.com>
2023-10-31 20:50:30 -0600

    test from local repo to remote-jiaqi

---
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SampleTest.java
@@ -0,0 +1,12 @@
+package com.thelocalmarketplace.software.test;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SampleTest {
+	
+	@Test
+	public void sampleTest() {
+		assertTrue(true);
+	}
+}
