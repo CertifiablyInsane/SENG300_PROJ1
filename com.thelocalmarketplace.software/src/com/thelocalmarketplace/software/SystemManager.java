0a53b7fc8f38d8047d5231531279819e674e7ed3
André Beaulieu <98614061+CertifiablyInsane@users.noreply.github.com>
2023-11-01 12:28:15 -0600

    Testing. Git isn't working.

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SystemManager.java
@@ -0,0 +1,9 @@
+package com.thelocalmarketplace.software;
+
+public class SystemManager {
+	private StartSession c_startSession;
+	
+	public void initialize() {
+		c_startSession = new StartSession();
+	}
+}
