08cc564eb2ddbe892d2776f49aa639397e75271d
André Beaulieu <98614061+CertifiablyInsane@users.noreply.github.com>
2023-10-31 19:12:26 -0600

    Initial Commit - Software

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SampleSoftware.java
@@ -0,0 +1,14 @@
+package com.thelocalmarketplace.software;
+
+import java.math.BigDecimal;
+
+// Proof that connections to hardware works
+import com.tdc.coin.*;
+
+public class SampleSoftware {
+	Coin coin = new Coin(new BigDecimal(1));
+	public static void main(String[] args) {
+		System.out.println("Hello, Gordon!");
+		
+	}
+}
