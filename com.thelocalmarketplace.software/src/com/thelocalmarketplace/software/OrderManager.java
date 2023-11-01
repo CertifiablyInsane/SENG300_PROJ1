5012baeaf90e7c5cfd252ac679f5da8cf7e39696
André Beaulieu <98614061+CertifiablyInsane@users.noreply.github.com>
2023-11-01 13:21:18 -0600

    Added Fundamental Managers. Established Main.java

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/OrderManager.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/OrderManager.java
@@ -1,11 +1,22 @@
 package com.thelocalmarketplace.software;
 
-public class OrderManager {
+import com.thelocalmarketplace.hardware.*;
 
-	public void addItem() {
-		
+// AB: Subeg, I created this class as the manager that will divert the
+// added item to whichever type of item is added. No functionality yet,
+// I just needed to be able to reference it.
+public class OrderManager {
+	public boolean enabled;
+
+	public OrderManager() {
+		System.out.println("OrderManager has been created");
 	}
-	public void removeItem() {
-		
+	
+	public void addItem(Product item) {
+		// AB: So here check if Product is of type BarcodedProduct, etc.
+	}
+	public void removeItem(Product item) {
+		// Not yet implemented
+		throw new UnsupportedOperationException();
 	}
 }
