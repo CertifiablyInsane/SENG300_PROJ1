058159068ee0c1b031661bde0d574ecdd1cde896
André Beaulieu <98614061+CertifiablyInsane@users.noreply.github.com>
2023-11-03 14:02:42 -0600

    Added WeightChecker. Redid Main to include Hardware.

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Main.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Main.java
@@ -3,13 +3,20 @@
 // test comment 
 package com.thelocalmarketplace.software;
 
+import com.thelocalmarketplace.hardware.SelfCheckoutStation;
+
+import powerutility.PowerGrid;
+
 // This is main!
 public class Main {
 	public static void main(String[] args) {
+		// Create an instance of all hardware, and enable it.
+		SelfCheckoutStation hardware = new SelfCheckoutStation();
+		hardware.plugIn(PowerGrid.instance());
+		hardware.turnOn();
 		// Create a system Manager and initialize it. 
 		// This system manager will store all the data and
 		// keep track of the active session.
-		SystemManager sysManager = new SystemManager();
-		sysManager.initialize();
+		SystemManager sysManager = new SystemManager(hardware);
 	}
 }
