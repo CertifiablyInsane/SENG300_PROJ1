fc6b52e697ed9331ec92ae55f83a5b9ccda2be2d
camila-hernandez <114267949+camila-hernandez@users.noreply.github.com>
2023-11-03 22:17:06 -0600

    Revert "Added check to throw NullPointerSimulationException if hardware is null"
    This reverts commit f621e7c0d29578490351e12d48c3f406efa32c37.

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SystemManager.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SystemManager.java
@@ -5,8 +5,6 @@ package com.thelocalmarketplace.software;
 
 import com.thelocalmarketplace.hardware.*;
 
-import ca.ucalgary.seng300.simulation.NullPointerSimulationException;
-
 public class SystemManager {
 	// Reference to the hardware, providing access to the SelfCheckoutStation. 
 	public SelfCheckoutStation hardware;
@@ -21,9 +19,6 @@ public class SystemManager {
 	
 	// Constructor for the SystemManager, initializes hardware and software components.
 	public SystemManager(SelfCheckoutStation hardware) {
-		if (hardware == null) {
-			throw new NullPointerSimulationException("The hardware is not properly initialized.");
-		}
 		this.hardware = hardware;
 		orderManager = new OrderManager();
 		paymentManager = new PaymentManager();
