adc6bb1350a6aa033810262042b8734008bd20ca
camila-hernandez <114267949+camila-hernandez@users.noreply.github.com>
2023-11-03 20:41:47 -0600

    Created setUp() method that will initialize fields before each test

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SystemManagerTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SystemManagerTest.java
@@ -2,10 +2,16 @@ package com.thelocalmarketplace.software.test;
 
 import com.thelocalmarketplace.hardware.SelfCheckoutStation;
 import com.thelocalmarketplace.software.SystemManager;
+import org.junit.*;
 
 public class SystemManagerTest {
 	
 	private SelfCheckoutStation hardware;
 	private SystemManager systemManager;
 	
+	@Before
+	public void setUp() {
+		hardware = new SelfCheckoutStation();
+		systemManager = new SystemManager(hardware); 
+	}
 }
