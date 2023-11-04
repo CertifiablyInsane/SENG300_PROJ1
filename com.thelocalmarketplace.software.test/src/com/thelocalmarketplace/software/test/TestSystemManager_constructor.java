fd24c29cde7e5cc9ca750d20b83c691a1962e7fd
camila-hernandez <114267949+camila-hernandez@users.noreply.github.com>
2023-11-04 11:55:08 -0600

    Added fields and setUp() method for TestSystemManager_constructor

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/TestSystemManager_constructor.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/TestSystemManager_constructor.java
@@ -1,5 +1,21 @@
 package com.thelocalmarketplace.software.test;
 
+import org.junit.Before;
+
+import com.thelocalmarketplace.hardware.SelfCheckoutStation;
+import com.thelocalmarketplace.software.SystemManager;
+
 public class TestSystemManager_constructor {
 
+	// Reference to the hardware, providing access to SelfCheckoutStation.
+	// Reference to the system manager, providing access to SystemManager.
+	private SelfCheckoutStation hardware;
+	private SystemManager systemManager;
+		
+	// Set up the test environment before each test method
+	@Before
+	public void setUp() {
+		hardware = new SelfCheckoutStation();
+		systemManager = new SystemManager(hardware); 
+	}
 }
