f96261dce0bcdc0d0da25f14962f5312997b1b5a
am1rawish <amira.wishah@ucalgary.ca>
2023-11-04 16:59:33 -0600

    fixing weird errors after fetching and merging main -- just syntax error

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithCoinScratch.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithCoinScratch.java
@@ -1,39 +1,3 @@
-package com.thelocalmarketplace.software;
-
-import java.math.BigDecimal;
-
-import com.tdc.coin.Coin;
-
-<<<<<<< HEAD
-//import com.jjjwelectronics.*;
-
-public class payWithCoinScratch {
-    private BigDecimal totalDue;
-    private float totalPaid;
-    private float totalDueFloat;
-
-    public payWithCoinScratch(BigDecimal totalDue) {
-        PaymentManager newSession = new PaymentManager(totalDue, 0);
-        totalPaid = newSession.getValuePaid();
-        totalDueFloat = newSession.getValueDue();
-    }
-
-    public void insertCoin(Coin testCoin) {
-        float coinTempValue = testCoin.getValue().floatValue();
-        totalPaid = totalPaid + coinTempValue;
-        System.out.println("Inserted " + testCoin.toString() + ". Total paid: " + totalPaid + " cents.");
-    }
-
-    public boolean isPaymentComplete() {
-        return totalPaid >= totalDueFloat;
-    }
-
-    // Get the remaining amount due
-    public float getAmountDue() {
-        return totalDueFloat - totalPaid;
-    }
-}
-=======
 import com.thelocalmarketplace.software.*;
 
 //import com.jjjwelectronics.*;
@@ -68,4 +32,4 @@ public class PayWithCoinScratch {
 		return totalPaid - totalDueFloat;
 	}
 }
->>>>>>> refs/remotes/origin/main
+
