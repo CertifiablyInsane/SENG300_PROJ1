b552a5c35889c037a1f2ac021862a4010412fc89
camila-hernandez <114267949+camila-hernandez@users.noreply.github.com>
2023-11-04 22:47:40 -0600

    Added test case for TestOrderManager_itemPlacedInBaggingArea

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/TestOrderManager_itemPlacedInBaggingArea.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/TestOrderManager_itemPlacedInBaggingArea.java
@@ -1,5 +1,21 @@
 package com.thelocalmarketplace.software.test;
+import org.junit.*;
+
+import com.thelocalmarketplace.software.OrderManager;
+
+import static org.junit.Assert.*;
 
 public class TestOrderManager_itemPlacedInBaggingArea {
 
+	private OrderManager orderManager; 
+	
+	// Test method to verify that the itemPlacedInBaggingArea method sets the readyForNewItem 
+	// flag to true when it is initially false and an item is placed in the bagging area
+	@Test
+    public void testItemPlacedInBaggingArea() {
+        orderManager.readyForNewItem = false;
+        orderManager.itemPlacedInBaggingArea();
+        // Verify that itemPlacedInBaggingArea sets readyForNewItem to true
+        assertTrue(orderManager.readyForNewItem);
+    }
 }
