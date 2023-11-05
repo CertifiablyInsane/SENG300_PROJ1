afdf744d38f380442de72c8085ffed97ed7b3816
jw1804 <124788631+jw1804@users.noreply.github.com>
2023-11-04 18:21:08 -0600

    fixed paymentManager

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/OrderManager.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/OrderManager.java
@@ -14,57 +14,57 @@ import ca.ucalgary.seng300.simulation.InvalidArgumentSimulationException;
 // AB: Subeg, I created this class as the manager that will divert the
 // added item to whichever type of item is added. No functionality yet,
 // I just needed to be able to reference it.
-public class OrderManager {
+public class OrderManager 
+{
 	public boolean enabled;
 	private boolean readyForNewItem = true;
 	public Mass expectedMass = new Mass(0);
 	public ArrayList<Item> items = new ArrayList<>();
 	private WeightChecker weightChecker;
-	private ItemAdderBarcode itemAdderBarcode;
-
-	public OrderManager(WeightChecker weightChecker) {
+	private ItemAdderBarcode itemAdderBarcode; 
+	
+	public OrderManager(WeightChecker weightChecker) 
+	{
 		System.out.println("OrderManager has been created");
 		this.weightChecker = weightChecker;
-
+		
 		itemAdderBarcode = new ItemAdderBarcode(this);
 	}
-
-	public void addItem(Item item) {
-		if (!enabled) {
-			return;
-		}
-
-		if (weightChecker.status() == ScaleStatus.BLOCKED) {
-			return;
-		}
-
-		if (!readyForNewItem) {
-			return;
-		}
-
+	
+	public void addItem(Item item)
+	{
+		if(!enabled) { return; }
+		
+		if(weightChecker.status() == ScaleStatus.BLOCKED){ return; }
+		
+		if(!readyForNewItem) { return; }
+		
 		readyForNewItem = false;
-
-		if (item instanceof BarcodedItem) {
-			BarcodedItem barcodedItem = (BarcodedItem) item;
+		
+		if(item instanceof BarcodedItem)
+		{
+			BarcodedItem barcodedItem = (BarcodedItem) item; 
 			itemAdderBarcode.addBarcodedItem(barcodedItem);
 		}
-
-		// SC: In future iterations the rest of the item types needed for add will be
-		// implemented
-		else {
-			throw new InvalidArgumentSimulationException("Item must be one of the specified types");
+		
+		// SC: In future iterations the rest of the item types needed for add will be implemented
+		else
+		{
+			throw new InvalidArgumentSimulationException("Item must be one of the specified types");		
 		}
-
+		
 		// SC: Temporary solution to signaling user, as there is no GUI
 		System.out.println("Please place item in bagging area");
 	}
-
-	public void removeItem(Product item) {
+	
+	public void removeItem(Product item) 
+	{
 		// Not yet implemented
 		throw new UnsupportedOperationException();
 	}
-
-	public void itemPlacedInBaggingArea() {
+	
+	public void itemPlacedInBaggingArea()
+	{
 		readyForNewItem = true;
 	}
 }
