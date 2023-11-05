76bb6b0a7edd265ccae5b339245502316738cd43
camila-hernandez <114267949+camila-hernandez@users.noreply.github.com>
2023-11-04 22:10:33 -0600

    Added comments to test suite TestOrderManager_constructor

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/TestOrderManager_constructor.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/TestOrderManager_constructor.java
@@ -4,22 +4,22 @@ package com.thelocalmarketplace.software.test;
 import org.junit.*;
 import static org.junit.Assert.*;
 
-import java.math.BigDecimal;
-
 import com.jjjwelectronics.scale.ElectronicScale;
-import com.thelocalmarketplace.software.ItemAdderBarcode;
 import com.thelocalmarketplace.software.OrderManager;
 import com.thelocalmarketplace.software.PaymentManager;
-import com.thelocalmarketplace.software.SystemManager;
 import com.thelocalmarketplace.software.WeightChecker;
 
 // This class is used to test the various functions of the OrderManager constructor:
 public class TestOrderManager_constructor {
 
+	// Reference to the weightChecker, providing access to WeightChecker.
+	// Reference to the paymentManager, providing access to PaymentManager.
+	// Reference to the orderManager, providing access to OrderManager.
 	private WeightChecker weightChecker;
 	private PaymentManager paymentManager;
 	private OrderManager orderManager; 
 	
+	// Set up the test environment before each test method
 	@Before
 	public void setup() {
 		ElectronicScale scaleToMonitor = new ElectronicScale();
