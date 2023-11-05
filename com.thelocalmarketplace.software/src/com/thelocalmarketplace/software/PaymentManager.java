04858dc0f066c74947ce18acc6b6e41e3a44b413
am1rawish <amira.wishah@ucalgary.ca>
2023-11-04 19:33:01 -0600

    fixed PaymentManager from merge

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PaymentManager.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PaymentManager.java
@@ -2,8 +2,6 @@
 
 package com.thelocalmarketplace.software;
 
-import java.math.BigDecimal;
-
 import com.tdc.coin.Coin;
 
 // AB: Jiaqi and Amira, I created this class as the manager that will
@@ -12,33 +10,10 @@ import com.tdc.coin.Coin;
 public class PaymentManager {
     public boolean enabled;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private float amountDueFloat;
-    private float amountPaid;
-
-    public PaymentManager(BigDecimal amountDue, float amountPaid) {
-        System.out.println("PaymentManager has been created");
-        amountDueFloat = amountDue.floatValue();
-        this.amountPaid = amountPaid;
-    }
-
-    public float getValueDue() {
-        return amountDueFloat;
-    }
-
-    public float getValuePaid() {
-        return amountPaid;
-    }
-}
-=======
-	private float amountDueFloat = 0;
-	private float amountPaid = 0;
-=======
 	public float amountDue = 0;
 	public float amountPaid = 0;
 	private PayWithCoin payWithCoin;
->>>>>>> refs/remotes/origin/main
+
 
 	public PaymentManager() {
 		System.out.println("Payment Manager has been created");
@@ -52,17 +27,5 @@ public class PaymentManager {
 		}
 	}
 
-<<<<<<< HEAD
-	public float getValueDue() {
-		return amountDueFloat - this.amountPaid;
-	}
-
-	public float getValuePaid() {
-		return amountPaid;
-	}
-
 }
->>>>>>> refs/remotes/origin/main
-=======
-}
->>>>>>> refs/remotes/origin/main
+
